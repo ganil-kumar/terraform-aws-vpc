d7cf094e7c7817a7768d6cb9a589ed37af547097
ganil-kumar <g.anilkumar3852@gmail.com>
2026-03-22 20:42:52 +0530

    vpc

--- variables.tf
+++ variables.tf
@@ -9,11 +9,21 @@ variable "environment" {
 variable "vpc_cidr" {
     type = string
     default = "10.0.0.0/16"
-  
+
 }
 
 variable "vpc_tags" {
     type = map 
     default = {}
+}
+
+variable "igw_tags" {
+    type = map 
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list 
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
   
 }
